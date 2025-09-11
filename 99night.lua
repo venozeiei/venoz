49fea294f5ef0cf8e039dc95a0cbf97145687361
venozeiei <armcraftwar@gmail.com>
2025-09-11 17:46:04 +0700

    Add files via upload

---
+++ 99night.lua
@@ -0,0 +1,10 @@
+setfpscap(25)
+_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
+    "NIL" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
+}
+_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
+_G.Max_Diamond = 10000 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
+
+_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
+script_key = "OeXUZaOKTIRzmVmJlUIeIsbtEySAYwla"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
