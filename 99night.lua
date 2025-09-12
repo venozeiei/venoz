1288e42e308f24b74191d313f9b60158ee1e6479
venozeiei <armcraftwar@gmail.com>
2025-09-12 15:30:29 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -1,32 +1,10 @@
-script_key="fTKmzjpidBPgYuPqUciLgzJYNXaTrdPb";
-getgenv().Configs = {
-    -- แจ้งเตือน ( Notify )
-    WebhookUrl = "", -- ลิ้ง Webhook Discord
-    WebhookOneTime = false, -- ถ้าเป็น true จะแจ้งเฉพาะตอน Diamond ถึงที่กำหนด
-    DisableLogRAM = true, -- ปิด Log ของ Account Manager
-
-    -- การฟาร์ม ( Farm )
-    LimitDiamond = 0, -- จำกัด Diamond ถ้าถึงกำหนดจะหยุดฟาร์ม / ถ้าเป็น 0 คือฟาร์มเรื่อยๆ
-    ServerTimeout = 10, -- กำหนดการหมดเวลายืนในเซิร์ฟ X วิให้ย้าย (ไม่จำเป็นมากเอาไว้กันบัคเท่านั้น)
-    ServerFindRange = 1, -- กำหนดจำนวนเซิร์ฟที่จะหาก่อนย้ายไปยังเซิร์ฟที่หาเจอ
-    AutoStronghold = true, -- ออโต้สตรองโฮลด์
-    MaxStrongholdLevel = 4, -- ระดับสูงสุดของสตรองโฮลด์
-    StrongholdFarmTime = 400, -- เวลาฟาร์มในสตรองโฮลด์
-
-    -- คลาส ( Class )
-    BuyClass = {"Gambler", "None"}, -- ชื้อคลาส
-    DoFirstTimeReroll = true, -- รีร้านคลาสครั้งแรกฟรี ถ้าไม่มีคลาสที่ต้องการขาย
-    AutoEquipClass = "Gambler", -- เลือกใส่ Class
-    UpgradeClass = false, -- อัพเวลคลาสอัตโนมัติ (ไม่ได้ทำเควส แค่อัพเวลตอนมันพร้อมเท่านั้น)
-
-    -- เพิ่มเติม ( Misc )
-    ServerHopProtection = true, -- เอาไว้กัน Ronix ไม่ย้ายเซิฟ
-
+setfpscap(20)
+_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
+    "Gambler" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
 }
-getgenv().GoogleSheetLog = {
-    -- Google Sheet
-    Enable = false, -- เปิดใช้ฟีเจอร์
-    WebAppURL = "" -- ใส่ URL ของ Google Apps Script ที่เชื่อมต่อกับ Google Sheets ของคุณ
+_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
+_G.Max_Diamond = 10000 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
 
-}
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Clehxb/7ea49f500b6e941f7b6332f6879cd059/raw/99NightLoader"))()
+_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
+script_key = "OeXUZaOKTIRzmVmJlUIeIsbtEySAYwla"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
