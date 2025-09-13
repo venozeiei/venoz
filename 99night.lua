b6e492cabacf2e5b12abd09e81155435793df362
venozeiei <armcraftwar@gmail.com>
2025-09-13 23:34:03 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -3,7 +3,7 @@ _G.WebhookLink = "LINK HERE"
 _G.OPFarmGem = true
 
 _G.Class = {
-    Enabled = true,
+    Enabled = false,
     Target = "Gambler" -- Class Name
 }
 
@@ -19,3 +19,4 @@ _G.LockDiamond = {
 -- Script Here !!!
 script_key="WQGQxHKbEXOwHCLsdjRexCxWJLXnyBBb";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
+
