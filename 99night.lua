7510c98bf508e737bd89841d112f3e38a5f6699a
venozeiei <armcraftwar@gmail.com>
2025-09-28 02:26:10 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -1,28 +1,39 @@
-getgenv().JinkX = {
-    ['99 Night Diamond Farm'] = {
-        ['Gems_Limit'] = 3000, -- Set your 'Gems' Limits
-        
-        ['Class'] = {
-            ['Target'] = {"Cyborg"}, -- Work only [Alien, Cyborg, Pyromaniac, Assassin]
-            
-            ['Enabled'] = true, -- Automatically buy your "Target" Class
-            ['AutoUpgrade'] = false, -- Automatically upgrade your "Target" Class
-            ['Level'] = 1, -- Level 1 - 3
-        },
+_G.WebhookLink = "LINK HERE"
+_G.OPFarmGem = true
 
-        -- Performance Settings
-        ['FPS_Boost'] = true,
-        ['FPS_Cap'] = false,
-        ['FPS_Value'] = 30,
-    },
+
+
+
+
+
+
+
+
+
+_G.Class = {
+    Enabled = true,
+    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
+
+
 }
-script_key="iNDBRWtITUTzDbVmZhmmcbFZBuTMVoGg";
-repeat wait() until game:IsLoaded()
 
-if game.GameId == 6701277882 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/d169661d1f368f4d900c9f1a477306da.lua"))()
-    return
-elseif game.GameId == 7326934954 then
-    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f7dfc0d64587003c292c68265b0ec9f7.lua"))()
-    return
-end
+
+
+_G.LockDiamond = {
+    Enabled = true,
+    Amount = 3000, -- 1000,2000,3000
+    SendWebhook = {
+        Enabled = true,
+        WebhookLink = "",
+        Message = "Reached Target", 
+    }
+}
+-- Log Roblox Account Manger PC ONLY -> TURN ON WEBSERVER IN DEVELOPER MODE AND CHANGE Enabled to true Enabled = true
+_G.LogRam = {
+    Enabled = false,
+    Port = 7963,
+    Debug = false, -- F9 CONSOLE
+}
+-- Script Here !!!
+script_key="WQGQxHKbEXOwHCLsdjRexCxWJLXnyBBb";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
