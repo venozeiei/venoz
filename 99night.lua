dd832f1f161f2e582d758b74442796610612535a
venozeiei <armcraftwar@gmail.com>
2025-10-08 08:05:38 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -1,13 +1,23 @@
-_G.Lock = { -- Cyborg , Assassin , Pyromaniac 
-    "Cyborg" -- ถ้าใส่ NIL ก็คือไม่ต้องซื้ออาชีพ เก็บเพชรยาวๆ
+_G.WebhookLink = "LINK HERE"
+_G.OPFarmGem = true
 
-
-
-
+_G.Class = {
+    Enabled = true,
+    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
 }
-_G.AddFriend = true -- เพิ่มเพื่อนออโต้ true / false
-_G.Max_Diamond = 10000 -- เมื่อเพชรครบ 10,000 จะหยุดฟาม
 
-_G.Webhook = "" -- ใส่ลิ้งค์Webhook เพื่อรับการแจ้งเตือนผลการฟามเพชรทุกๆ 300วินาที
-script_key = "orNYdeFquFSBvzHvogpBMieLQfIPRPxc"
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/aaa4dc9eee92182acc623f09bc4691ed.lua"))()
+_G.LockDiamond = {
+    Enabled = true,
+    Amount = 10000, -- 1000,2000,3000
+    SendWebhook = {
+        Enabled = true,
+        WebhookLink = "",
+        Message = "Reached Target", 
+    }
+}
+-- Log Account Hub link -> https://accounthub.bunnycat.xyz/login
+_G.ApiKey = "" -- get api in link
+-- Script Here !!!
+
+script_key="WQGQxHKbEXOwHCLsdjRexCxWJLXnyBBb";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
