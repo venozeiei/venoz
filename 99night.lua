de22d26cccadd32d65ee39b2de07184227a1ad84
venozeiei <armcraftwar@gmail.com>
2025-10-09 15:47:10 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -1,23 +1,21 @@
-_G.WebhookLink = "LINK HERE"
-_G.OPFarmGem = true
-
-_G.Class = {
-    Enabled = true,
-    Target = "Cyborg" -- Class Name or Target = {"Gambler" , "Cyborg"}
-}
-
-_G.LockDiamond = {
-    Enabled = true,
-    Amount = 10000, -- 1000,2000,3000
-    SendWebhook = {
-        Enabled = true,
-        WebhookLink = "",
-        Message = "Reached Target", 
+getgenv().Mode = "OneClick"
+getgenv().Setting = {
+    ["Team"] = "Pirates", -- Options "Pirates", "Marines"
+    ["Fruits"] = {  -- setting for fruits u want
+        ["Primary"] = { -- if current fruit is not in this list, eat/buy
+            "Dough-Dough",
+            "Buddha-Buddha",
+            -- u can configs add mores/remove and must end with , (comma symbol)
+        },
+        ["Normal"] = { -- it just a normal fruit list
+            "Ice-Ice",
+            "Light-Light",
+            "Dark-Dark",
+            "Magma-Magma",
+            -- u can configs add mores/remove and must end with , (comma symbol)
+        }
+        -- run this for get all fruit name local t={};for _,v in pairs(game.ReplicatedStorage.Remotes.CommF_:InvokeServer("GetFruits"))do table.insert(t,"{v.Name}")end;setclipboard(table.concat(t, "\n"))
     }
-}
--- Log Account Hub link -> https://accounthub.bunnycat.xyz/login
-_G.ApiKey = "" -- get api in link
--- Script Here !!!
+};
 
-script_key="WQGQxHKbEXOwHCLsdjRexCxWJLXnyBBb";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/5f69c589c2e08aee7d37c351dd3068af.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Loader.lua"))()
