675b700cc6b2c02ce0abdfa117d68f83b3e0683e
venozeiei <armcraftwar@gmail.com>
2026-03-27 18:08:01 +0700

    Update 99night.lua

--- 99night.lua
+++ 99night.lua
@@ -1,17 +1,16 @@
 repeat task.wait() until game:IsLoaded()
+repeat task.wait(1) until _G.Horst_SetDescription ~= nil -- รอ Horst พร้อม
 
 local Players = game:GetService("Players")
 local RS = game:GetService("ReplicatedStorage")
 local Plr = Players.LocalPlayer.Name
 
-local LV = workspace.Camera:WaitForChild(Plr).Head.NameLevelBBGUI.LevelFrame.TextLabel.Text
-local level = tonumber(string.match(LV,"%d+"))
-
-local Gems = Players.LocalPlayer.Backpack.Framework.TasksV2.TaskCard.TaskCardTemplate.ClaimButton.GemsAmount.Text
-local stardust = Players.LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Stardust.Amount.Text
-local Gold = Players.LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Coins.Amount.Text
-
 while true do
+    local LV = workspace.Camera:WaitForChild(Plr).Head.NameLevelBBGUI.LevelFrame.TextLabel.Text
+    local level = tonumber(string.match(LV,"%d+"))
+    local Gems = Players.LocalPlayer.Backpack.Framework.TasksV2.TaskCard.TaskCardTemplate.ClaimButton.GemsAmount.Text
+    local stardust = Players.LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Stardust.Amount.Text
+    local Gold = Players.LocalPlayer.PlayerGui.HUD.BottomFrame.CurrencyList.Coins.Amount.Text
     local mapName = RS.Map.Value
     local challengeMap = RS.ChallengeMap.Value
     local wave = Players.LocalPlayer.PlayerGui.HUD.Wave.Text
